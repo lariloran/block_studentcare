d6072f9ecbbfa5e8841462b844dc09fb39c154ce
Rafael Lariloran <lariloran2@gmail.com>
2024-11-16 10:18:03 -0300

    Alterando prazer para alegria(correto)

--- db/install.php
+++ db/install.php
@@ -19,7 +19,7 @@ foreach ($records_aeq_class as $record) {
 
 // EMOTIONS-CLASS INSERTS.
 $records_emotions = [
-    (object)['id' => 1, 'classeaeq_id' => 1, 'nome' => 'Prazer', 'txttooltip' => 'Uma sensação de satisfação e bem-estar, normalmente associada a experiências agradáveis e positivas.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
+    (object)['id' => 1, 'classeaeq_id' => 1, 'nome' => 'Alegria', 'txttooltip' => 'Uma sensação de satisfação e bem-estar, normalmente associada a experiências agradáveis e positivas.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 2, 'classeaeq_id' => 1, 'nome' => 'Esperança', 'txttooltip' => 'Um sentimento otimista sobre o futuro, acreditando que algo bom vai acontecer.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 3, 'classeaeq_id' => 1, 'nome' => 'Orgulho', 'txttooltip' => 'Um sentimento de satisfação consigo mesmo ou com os outros por realizações, sucesso ou habilidades.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 4, 'classeaeq_id' => 1, 'nome' => 'Raiva', 'txttooltip' => 'Uma emoção intensa, muitas vezes resultante de frustração ou injustiça, que pode levar a ações impulsivas.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
@@ -27,7 +27,7 @@ $records_emotions = [
     (object)['id' => 6, 'classeaeq_id' => 1, 'nome' => 'Vergonha', 'txttooltip' => 'Um sentimento desconfortável ou doloroso causado pela percepção de que algo que você fez ou disse foi errado ou embaraçoso.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 7, 'classeaeq_id' => 1, 'nome' => 'Desespero', 'txttooltip' => 'Um sentimento de completa falta de esperança, onde parece que não há soluções ou saídas para uma situação difícil.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 8, 'classeaeq_id' => 1, 'nome' => 'Tédio', 'txttooltip' => 'Um estado de falta de interesse ou estímulo, muitas vezes associado à repetição ou à ausência de desafios.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
-    (object)['id' => 9, 'classeaeq_id' => 2, 'nome' => 'Prazer', 'txttooltip' => 'Uma sensação de satisfação e bem-estar, normalmente associada a experiências agradáveis e positivas.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
+    (object)['id' => 9, 'classeaeq_id' => 2, 'nome' => 'Alegria', 'txttooltip' => 'Uma sensação de satisfação e bem-estar, normalmente associada a experiências agradáveis e positivas.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 10, 'classeaeq_id' => 2, 'nome' => 'Esperança', 'txttooltip' => 'Um sentimento otimista sobre o futuro, acreditando que algo bom vai acontecer.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 11, 'classeaeq_id' => 2, 'nome' => 'Orgulho', 'txttooltip' => 'Um sentimento de satisfação consigo mesmo ou com os outros por realizações, sucesso ou habilidades.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 12, 'classeaeq_id' => 2, 'nome' => 'Raiva', 'txttooltip' => 'Uma emoção intensa, muitas vezes resultante de frustração ou injustiça, que pode levar a ações impulsivas.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
@@ -35,7 +35,7 @@ $records_emotions = [
     (object)['id' => 14, 'classeaeq_id' => 2, 'nome' => 'Vergonha', 'txttooltip' => 'Um sentimento desconfortável ou doloroso causado pela percepção de que algo que você fez ou disse foi errado ou embaraçoso.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 15, 'classeaeq_id' => 2, 'nome' => 'Desespero', 'txttooltip' => 'Um sentimento de completa falta de esperança, onde parece que não há soluções ou saídas para uma situação difícil.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 16, 'classeaeq_id' => 2, 'nome' => 'Tédio', 'txttooltip' => 'Um estado de falta de interesse ou estímulo, muitas vezes associado à repetição ou à ausência de desafios.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
-    (object)['id' => 17, 'classeaeq_id' => 3, 'nome' => 'Prazer', 'txttooltip' => 'Uma sensação de satisfação e bem-estar, normalmente associada a experiências agradáveis e positivas.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
+    (object)['id' => 17, 'classeaeq_id' => 3, 'nome' => 'Alegria', 'txttooltip' => 'Uma sensação de satisfação e bem-estar, normalmente associada a experiências agradáveis e positivas.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 18, 'classeaeq_id' => 3, 'nome' => 'Esperança', 'txttooltip' => 'Um sentimento otimista sobre o futuro, acreditando que algo bom vai acontecer.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 19, 'classeaeq_id' => 3, 'nome' => 'Orgulho', 'txttooltip' => 'Um sentimento de satisfação consigo mesmo ou com os outros por realizações, sucesso ou habilidades.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
     (object)['id' => 20, 'classeaeq_id' => 3, 'nome' => 'Alívio', 'txttooltip' => 'Um sentimento de tranquilidade e conforto que surge quando uma situação estressante, difícil ou dolorosa chega ao fim ou é resolvida.', 'antes' => 1, 'durante' => 1, 'depois' => 1],
