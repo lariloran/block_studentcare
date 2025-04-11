c91ce359663262f833e36f9a3e633026b284dfcc
marceloschmitt <marcelo.schmitt@poa.ifrs.edu.br>
2025-04-11 15:16:58 -0300

    Add Copyright line.

--- Download.php
+++ Download.php
@@ -23,29 +23,29 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(__DIR__ . '/../../config.php'); 
-require_once('collection_manager.php'); 
+require_once(__DIR__ . '/../../config.php');
+require_once('collection_manager.php');
 
-$coleta_id = required_param('coleta_id', PARAM_INT); 
+$coleta_id = required_param('coleta_id', PARAM_INT);
 
 require_login();
 
-$courseid = optional_param('courseid', 0, PARAM_INT); 
+$courseid = optional_param('courseid', 0, PARAM_INT);
 if ($courseid) {
-    $context = context_course::instance($courseid); 
+    $context = context_course::instance($courseid);
 } else {
-    $context = context_system::instance(); 
+    $context = context_system::instance();
 }
-$PAGE->set_context($context); 
+$PAGE->set_context($context);
 
 ob_clean();
 
-$manager = new collection_manager(); 
+$manager = new collection_manager();
 
 if (isset($_GET['format']) && $_GET['format'] === 'json') {
-    $manager->download_json($coleta_id); 
+    $manager->download_json($coleta_id);
 } elseif (isset($_GET['format']) && $_GET['format'] === 'csv') {
-    $manager->download_csv($coleta_id); 
+    $manager->download_csv($coleta_id);
 } else {
     echo "Formato de download inválido.";
     exit();
