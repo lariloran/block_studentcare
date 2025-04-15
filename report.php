f17c155c7f8d9e2624a687f085ccd4204bbf696e
marceloschmitt <marcelo.schmitt@poa.ifrs.edu.br>
2025-04-15 16:42:19 -0300

    Codechecker fix.

--- report.php
+++ report.php
@@ -1,8 +1,5 @@
 <?php
-require_once('../../config.php');
-require_login();
-
-$context = cont// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -24,6 +21,9 @@ $context = cont// This file is part of Moodle - http://moodle.org/
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @package block_studentcare
  */
+
+require_once('../../config.php');
+require_login();
 
 $context = context_course::instance($COURSE->id);
 $PAGE->set_url('/blocks/studentcare/report.php');
